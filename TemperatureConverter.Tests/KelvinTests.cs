ca7799441b190a9ed3f1c179231368f1cd7bce5e
Redas Kriksciukaitis <redas.kriksciukaitis@oag.com>
2022-01-31 16:13:11 +0200

    Add unit tests

---
+++ TemperatureConverter.Tests/KelvinTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TemperatureConverter.Tests;
+
+[TestClass]
+public class KelvinTests
+{
+    private Kelvin kelvin;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        kelvin = new Kelvin();
+    }
+    
+    [TestMethod]
+    public void ConvertsTo_Kelvin()
+    {
+        var result = kelvin.ConvertToKelvin(100);
+
+        result.Should().Be(100M);
+    }
+    
+    [TestMethod]
+    public void ConvertsTo_Fahrenheit()
+    {
+        var result = kelvin.ConvertToFahrenheit(100);
+
+        result.Should().Be(-279.67M);
+    }
+    
+    [TestMethod]
+    public void ConvertsTo_Celsius()
+    {
+        var result = kelvin.ConvertToCelsius(100);
+
+        result.Should().Be(-173.15M);
+    }
+}
