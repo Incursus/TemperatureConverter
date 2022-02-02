356e643d3e94e7cb1cbfec57d0af9ba3eae6759c
Redas Kriksciukaitis <redas.kriksciukaitis@oag.com>
2022-02-02 13:15:47 +0200

    Update unit tests

---
+++ TemperatureConverter.Tests/ConverterTests.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TemperatureConverter.Tests;
+
+[TestClass]
+public class ConverterTests
+{
+    [TestMethod]
+    public void ConvertsFromCelsiusToKelvin()
+    {
+        var converter = new CelsiusToKelvinConverter();
+
+        var result = converter.Convert(100);
+        
+        result.Should().Be(373.15M);
+    }
+    
+    [TestMethod]
+    public void ConvertsFromCelsiusToFahrenheit()
+    {
+        var converter = new CelsiusToFahrenheitConverter();
+
+        var result = converter.Convert(100);
+        
+        result.Should().Be(212M);
+    }
+    
+    [TestMethod]
+    public void ConvertsFromKelvinToCelsius()
+    {
+        var converter = new KelvinToCelsiusConverter();
+
+        var result = converter.Convert(100);
+        
+        result.Should().Be(-173.15M);
+    }
+    
+    [TestMethod]
+    public void ConvertsFromKelvinToFahrenheit()
+    {
+        var converter = new KelvinToFahrenheitConverter();
+
+        var result = converter.Convert(100);
+        
+        result.Should().Be(-279.67M);
+    }
+    
+    [TestMethod]
+    public void ConvertsFromFahrenheitToCelsius()
+    {
+        var converter = new FahrenheitToCelsiusConverter();
+
+        var result = converter.Convert(451);
+        
+        result.Should().Be(232.78M);
+    }
+    
+    [TestMethod]
+    public void ConvertsFromFahrenheitToKelvin()
+    {
+        var converter = new FahrenheitToKelvinConverter();
+
+        var result = converter.Convert(451);
+        
+        result.Should().Be(506.28M);
+    }
+}
---
+++ TemperatureConverter.Tests/TemperatureConverterResolverTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TemperatureConverter.Tests;
+
+[TestClass]
+public class TemperatureConverterResolverTests
+{
+    [TestMethod]
+    public void GetsMatchedConverterWhenFound()
+    {
+        var resolver = new TemperatureConverterResolver();
+
+        var result = resolver.Get(new List<ITemperatureConverter?>() {new FahrenheitToKelvinConverter(), new CelsiusToFahrenheitConverter()}, "C", "F");
+
+        result.Should().NotBeNull();
+        result.Should().BeOfType<CelsiusToFahrenheitConverter>();
+    }
+    
+    [TestMethod]
+    public void GetsNullConverterWhenNotFound()
+    {
+        var resolver = new TemperatureConverterResolver();
+
+        var result = resolver.Get(new List<ITemperatureConverter?>() {new CelsiusToFahrenheitConverter()}, "Z", "Z");
+
+        result.Should().BeNull();
+    }
+}
